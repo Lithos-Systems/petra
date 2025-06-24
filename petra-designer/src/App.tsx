babd996539b63d3d4e90f5965b433fb3b578e86e
nstone-lithos <nathan@lithos.systems>
2025-06-24 15:10:52 -0500

    Create App.tsx

---
+++ petra-designer/src/App.tsx
@@ -0,0 +1,56 @@
+import { ReactFlow, Background, Controls, MiniMap, Panel } from '@xyflow/react'
+import { Toaster } from 'react-hot-toast'
+import { useFlowStore } from './store/flowStore'
+import { nodeTypes } from './nodes'
+import { edgeTypes } from './edges'
+import Sidebar from './components/Sidebar'
+import PropertiesPanel from './components/PropertiesPanel'
+import YamlPreview from './components/YamlPreview'
+import Toolbar from './components/Toolbar'
+
+function App() {
+  const {
+    nodes,
+    edges,
+    onNodesChange,
+    onEdgesChange,
+    onConnect,
+    selectedNode,
+  } = useFlowStore()
+
+  return (
+    <div className="h-screen flex flex-col bg-gray-50">
+      <Toaster position="top-right" />
+      <Toolbar />
+      
+      <div className="flex-1 flex">
+        <Sidebar />
+        
+        <div className="flex-1 relative">
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            nodeTypes={nodeTypes}
+            edgeTypes={edgeTypes}
+            fitView
+            className="bg-gray-50"
+          >
+            <Background variant="dots" gap={20} className="bg-gray-50" />
+            <Controls />
+            <MiniMap className="bg-white" />
+          </ReactFlow>
+        </div>
+        
+        <div className="flex">
+          {selectedNode && <PropertiesPanel />}
+          <YamlPreview />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
