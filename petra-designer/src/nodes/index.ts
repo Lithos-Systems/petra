079626fdfdc574ce796957a54564242272559f34
nstone-lithos <nathan@lithos.systems>
2025-06-25 13:55:15 -0500

    Update index.ts

--- petra-designer/src/nodes/index.ts
+++ petra-designer/src/nodes/index.ts
@@ -1,17 +1,14 @@
-// src/nodes/index.ts
 import type { NodeTypes } from '@xyflow/react'
-import type { PetraNode } from '@/types/nodes'
-
 import SignalNode from './SignalNode'
 import BlockNode from './BlockNode'
 import TwilioNode from './TwilioNode'
 import MqttNode from './MqttNode'
 import S7Node from './S7Node'
 
-export const nodeTypes = {
+export const nodeTypes: NodeTypes = {
   signal: SignalNode,
   block: BlockNode,
   twilio: TwilioNode,
   mqtt: MqttNode,
   s7: S7Node,
-} satisfies NodeTypes
+}
