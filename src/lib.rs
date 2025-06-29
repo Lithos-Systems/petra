6329bd080a223f20fd11eaeba658e225ac5b9d9c
Nathan Stone <nathan@lithos.systems>
2025-06-29 14:15:56 -0500

    Fix Cargo manifest optional dev deps

--- src/lib.rs
+++ src/lib.rs
@@ -75,6 +75,7 @@ pub use history::{HistoryManager, HistoryConfig, SignalHistory};
 #[cfg(feature = "s7-support")]
 pub use s7::{S7Connector, S7Config, S7Mapping, S7Area, S7DataType, Direction};
 
+#[cfg(feature = "web")]
 pub use twilio::{TwilioConnector, TwilioConfig, TwilioAction, TwilioActionType};
 
 #[cfg(feature = "opcua-support")]
