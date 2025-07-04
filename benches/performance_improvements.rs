6fd1e1fed601ed909a5a41658c46a3245c958efb
Nathan Stone <nathan@lithos.systems>
2025-07-04 02:22:37 -0500

    Create performance_improvements.rs

---
+++ benches/performance_improvements.rs
@@ -0,0 +1,66 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
+use petra::*;
+
+fn benchmark_parallel_vs_sequential(c: &mut Criterion) {
+    let mut group = c.benchmark_group("parallel_execution");
+    
+    for block_count in [10, 50, 100, 500, 1000].iter() {
+        // Sequential execution
+        group.bench_with_input(
+            BenchmarkId::new("sequential", block_count),
+            block_count,
+            |b, &count| {
+                let config = create_benchmark_config_sequential(count);
+                let engine = Engine::new(config).unwrap();
+                b.iter(|| {
+                    black_box(engine.execute_scan_cycle());
+                });
+            },
+        );
+        
+        // Parallel execution
+        group.bench_with_input(
+            BenchmarkId::new("parallel", block_count),
+            block_count,
+            |b, &count| {
+                let config = create_benchmark_config_parallel(count);
+                let engine = Engine::new(config).unwrap();
+                b.iter(|| {
+                    black_box(engine.execute_scan_cycle());
+                });
+            },
+        );
+    }
+    
+    group.finish();
+}
+
+fn benchmark_simd_math(c: &mut Criterion) {
+    let mut group = c.benchmark_group("simd_math");
+    
+    for array_size in [100, 1000, 10000].iter() {
+        group.bench_with_input(
+            BenchmarkId::new("standard_add", array_size),
+            array_size,
+            |b, &size| {
+                let arrays = create_float_arrays(size);
+                b.iter(|| {
+                    standard_array_add(&arrays.0, &arrays.1)
+                });
+            },
+        );
+        
+        group.bench_with_input(
+            BenchmarkId::new("simd_add", array_size),
+            array_size,
+            |b, &size| {
+                let arrays = create_float_arrays(size);
+                b.iter(|| {
+                    simd_array_add(&arrays.0, &arrays.1)
+                });
+            },
+        );
+    }
+    
+    group.finish();
+}
