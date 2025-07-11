b74710b9ccdce4361c3d4b6d0daafe60c1e956b6
Nathan Stone <nathan@lithos.systems>
2025-07-11 00:39:26 -0500

    Update main.tsx

--- petra-designer/src/main.tsx
+++ petra-designer/src/main.tsx
@@ -4,8 +4,4 @@ import App from './App'
 import './styles/index.css'
 import '@xyflow/react/dist/style.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  // React Flow performs poorly with StrictMode due to double renders
-  // in development. Avoid wrapping the app with React.StrictMode
-  <App />,
-)
+
